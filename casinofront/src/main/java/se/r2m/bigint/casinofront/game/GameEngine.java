2fa328768c43f8022924843ad5fbce84526340ec
Bjorn Frantzen <bjorn.frantzen@cuemind.com>
2015-02-20 21:48:19 +0100

    Rename of storefront

--- casinofront/src/main/java/se/r2m/bigint/casinofront/game/GameEngine.java
+++
@@ -1,7 +0,0 @@
-package se.r2m.bigint.casinofront.game;
-
-public interface GameEngine {
-
-    GameOutput doPlay(GameInput input);
-
-}
--- casinofront/src/main/resources/README.txt
+++
@@ -1,2 +0,0 @@
-To run:
-mvn package && java -jar target\casinofront-0.0.1-SNAPSHOT.jar
